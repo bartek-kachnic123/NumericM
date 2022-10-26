5567c568e261711f48f08d025b6bdf414a5e0f20
Bartek Kachnic <bartek.kachnic123@gmail.com>
2022-10-26 23:18:04 +0200

    added Num2

---
+++ Num2/README.txt
@@ -0,0 +1,31 @@
+========================================================================
+Autor: Bartłomiej Kachnic,                           Krakow, 26.10.2022
+========================================================================
+
+* Zawartosc:
+============
+
+Katalog NUM2 zawiera:
+--------------------------------------------------------------------
+
+I.     Program obliczający rozwiązania dla macierzy A1 i A2, wektorów b i b'.
+        A takze normę róznic rozwiązań. Program wczytuje macierze z pliku txt
+        podanych w argumentach uruchomienia programu.
+
+       Program sklada sie z 1 pliku w jezyku python
+            1) Num2.py - program glowny
+            2) raport.pdf
+            3) pliki *.txt z danymi macierzy
+
+------------------------------------------------------------------------
+
+* Jak uruchomic program:
+=========================
+
+-> Aby uruchomic 1-szy program, nalezy wykonac komende:
+   a)   python3 num2.py A_1.txt A_2.txt B.txt
+  
+
+========================================================================
+
+
--- Num2/num2.py
+++ Num2/num2.py
@@ -1,32 +1,67 @@
+from math import fabs
 import numpy as np
+import sys
+def matrixFromFile(filename, separator=' '):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            matrix = []
+            lines = file.readlines()
+            
+            if len(list(lines)) == 1:
+                return list(map(float, lines[0].split(separator)))
 
-A_1 = [
-[2.34332898, -0.11253278, -0.01485349, 0.33316649, 0.71319625],
-[-0.11253278, 1.67773628, -0.32678856, -0.31118836, -0.43342631],
-[-0.01485349, -0.32678856, 2.66011353, 0.85462464, 0.16698798],
-[0.33316649, -0.31118836, 0.85462464, 1.54788582, 0.32269197],
-[0.71319625, -0.43342631, 0.16698798 ,0.32269197 ,3.27093538]
-]
-A_2 = [
-[2.34065520, -0.05353743, 0.00237792, 0.32944082, 0.72776588],
-[-0.05353743, 0.37604149, -0.70698859, -0.22898376 ,-0.75489595],
-[0.00237792 ,-0.70698859 ,2.54906441 ,0.87863502 ,0.07309288],
-[0.32944082 ,-0.22898376, 0.87863502 ,1.54269444, 0.34299341],
-[0.72776588 ,-0.75489595 ,0.07309288, 0.34299341 ,3.19154447]
-]
+            for line in lines:
+                matrix.append(list(map(float, line.split(separator))))
 
-b =  [3.55652063354463, -1.86337418741501, 5.84125684808554, -1.74587299057388, 0.84299677124244]
+        return matrix
+    except FileNotFoundError:
+        print(f'Plik o nazwie {filename} nie istnieje!')
 
-bPrim = b.copy()
-bPrim[0] = bPrim[0] + 1e-5
-y_1 = np.linalg.solve(A_1, b)
-y_2 = np.linalg.solve(A_2, b)
-y_1Prim = np.linalg.solve(A_1, bPrim)
-y_2Prim = np.linalg.solve(A_2, bPrim)
-print("y1: ", y_1, " y1 Prim: ", y_1Prim)
-print("y2: ", y_2, "y2 Prim: ", y_2Prim)
-norm_1 = np.linalg.norm(np.subtract(y_1, y_1Prim))
-norm_2 = np.linalg.norm(np.subtract(y_2, y_2Prim))
 
-print("Norm y1 - y1 Prim : ", norm_1)
-print("Norm y2 - y2 Prim: ", norm_2)
+
+
+def main():
+
+    if len(sys.argv) != 4:
+        print('Wrong number of args! Example: python3 num2.py A_1.txt A_2.txt B.txt')
+        exit(1)
+
+    A_1 = matrixFromFile(sys.argv[1])
+    A_2 = matrixFromFile(sys.argv[2])
+    B = matrixFromFile(sys.argv[3])
+
+    bPrim = B.copy()
+    bPrim[0] = bPrim[0] + 1e-5
+
+    y_1 = np.linalg.solve(A_1, B) 
+    y_2 = np.linalg.solve(A_2, B)
+
+    y_1Prim = np.linalg.solve(A_1, bPrim)
+    y_2Prim = np.linalg.solve(A_2, bPrim)
+    
+    print("Rozwiazanie A_1 * y1 = b  \ty1:  ", y_1)
+    print("Rozwiazanie A_1 * y'1 = b' \ty'1: ", y_1Prim)
+    print("Rozwiazanie A_2 * y2 = b \ty2:  ", y_2)
+    print("Rozwiazanie A_2 * y'2 = b' \ty'2: ", y_2Prim)
+
+    norm_1 = np.linalg.norm(np.subtract(y_1, y_1Prim))
+    norm_2 = np.linalg.norm(np.subtract(y_2, y_2Prim))
+
+    print("||y1-y'1|| = ", norm_1)
+    print("||y2-y'2|| = ", norm_2)
+
+
+    print(np.linalg.norm(A_1) * np.linalg.norm(np.linalg.inv(A_1)))
+    print(np.linalg.norm(A_2) * np.linalg.norm(np.linalg.inv(A_2)))
+
+  
+
+
+    
+
+
+
+
+
+if __name__ == '__main__':
+    main()
