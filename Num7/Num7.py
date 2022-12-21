04d9a29526f050ecea48c968c7b262e7e79c401e
bartlomiej.kachnic@student.uj.edu.pl <you@example.com>
2022-12-21 22:00:04 +0100

    Added lagrange function

--- Num7/Num7.py
+++ Num7/Num7.py
@@ -1,40 +1,137 @@
 import numpy as np
+from matplotlib import pyplot as plt
+import sys
 
-def y(x):
+def func(x):
     return 1 / (1 + 25 * np.power(x, 2))
 
-def first_interpolation(n, filename):
-    file = open(filename, "w", encoding='utf-8')
-    file.write("N;W(N)\n")
-    for i in range(n+1): # from 0 to n
-        x = -1 + (2 * (i / (n + 1)))
-        result = y(x)
-        file.write(f'{i};{result}\n')
+def first_Func(n):
+    x = np.zeros(n)
+    for i in range(n): # from 0 to n - 1
+        x[i] = -1 + (2 * (i / (n + 1)))
+    y = np.array(list(map(func, x)))
 
-    file.close()
+    return x, y
+        
 
-def second_interpolation(n, filename):
-    file = open(filename, "w", encoding='utf-8')
-    file.write("N;W(N)\n")
-    for i in range(n+1): # from 0 to n
-        x = np.cos((2*i + 1) / (2 * (n + 1)))
-        result = y(x)
-        file.write(f'{i};{result}\n')
+def langrage_interpolation(x_points, y_points, x):
+    n = len(x_points)
+    results = y_points.copy()
+    for i in range(n):
+        for j in range(n):
+            if (x_points[i] != x_points[j]):
+                results[i] *= (x - x_points[j])
+                results[i] /= (x_points[i] - x_points[j])
+    return sum(results)
 
-    file.close()
+def calculate_points1():
+    nSets = (5, 10, 25, 50)
+    colors = ("red", "green", "blue", "black")
+    pointsX = np.linspace(-1.0, 1.0, 256)
+    pointsX = np.array(pointsX)
+    pointsY = np.zeros_like(pointsX)
+    pointsY2 = np.zeros_like(pointsX)
+    pointsY3 = np.zeros_like(pointsX)
+    pointsY4 = np.zeros_like(pointsX)
+
+    xp, yp = first_Func(nSets[0])
+    for i in range(len(pointsX)):
+        pointsY[i] = langrage_interpolation(xp, yp, pointsX[i])
+
+    xp, yp = first_Func(nSets[1])
+    for i in range(len(pointsX)):
+        pointsY2[i] = langrage_interpolation(xp, yp, pointsX[i])
+
+    xp, yp = first_Func(nSets[2])
+    for i in range(len(pointsX)):
+        pointsY3[i] = langrage_interpolation(xp, yp, pointsX[i])
+
+    xp, yp = first_Func(nSets[3])
+    for i in range(len(pointsX)):
+        pointsY4[i] = langrage_interpolation(xp, yp, pointsX[i])
+        
+    plt.plot(pointsX, pointsY, label=f"N={nSets[0]}", color=colors[0])
+    plt.plot(pointsX, pointsY2, label=f"N={nSets[1]}", color=colors[1])
+    plt.plot(pointsX, pointsY3, label=f"N={nSets[2]}", color=colors[2])
+    plt.plot(pointsX, pointsY4, label=f"N={nSets[3]}", color=colors[3])
+
+    plt.title("Iterpolacja Wielomianowa Langranga")
+    plt.legend()
+
+    plt.xlabel("x")
+    plt.ylabel("W(x)")
+
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.show()
+
+
+def second_Func(n):
+    x = np.zeros(n)
+    pi = 3.14
+    for i in range(n): # from 0 to n
+        x[i] = np.cos(((2*i + 1) / (2 * (n + 1))) * pi)
+    y = np.array(list(map(func, x)))
+
+    return x, y
+
+def calculate_points2():
+    nSets = (5, 10, 25, 50)
+    colors = ("red", "green", "blue", "black")
+    pointsX = np.linspace(-1.0, 1.0, 256)
+    pointsY = np.zeros_like(pointsX)
+    pointsY2 = np.zeros_like(pointsX)
+    pointsY3 = np.zeros_like(pointsX)
+    pointsY4 = np.zeros_like(pointsX)
+
+    xp, yp = second_Func(nSets[0])
+    for i in range(len(pointsX)):
+        pointsY[i] = langrage_interpolation(xp, yp, pointsX[i])
+
+    xp, yp = second_Func(nSets[1])
+    for i in range(len(pointsX)):
+        pointsY2[i] = langrage_interpolation(xp, yp, pointsX[i])
+
+    xp, yp = second_Func(nSets[2])
+    for i in range(len(pointsX)):
+        pointsY3[i] = langrage_interpolation(xp, yp, pointsX[i])
+
+    xp, yp = second_Func(nSets[3])
+    for i in range(len(pointsX)):
+        pointsY4[i] = langrage_interpolation(xp, yp, pointsX[i])
+        
+    plt.plot(pointsX, pointsY, label=f"N={nSets[0]}", color=colors[0])
+    plt.plot(pointsX, pointsY2, label=f"N={nSets[1]}", color=colors[1])
+    plt.plot(pointsX, pointsY3, label=f"N={nSets[2]}", color=colors[2])
+    plt.plot(pointsX, pointsY4, label=f"N={nSets[3]}", color=colors[3])
+
+    plt.title("Iterpolacja Wielomianowa Langranga")
+    plt.legend()
+
+    plt.xlabel("x")
+    plt.ylabel("W(x)")
+    
+    
+    plt.xscale('log')
+    plt.yscale('log')
+
+    
+    plt.show()
 
 def main():
-    nSets = (5, 10, 25, 50)
-    filenamesA = (f'a{i}.csv' for i in range(1, 5))
-    filenamesB = (f'b{i}.csv' for i in range(1, 5))
-
-    dataA = tuple(zip(nSets, filenamesA))
-    dataB = tuple(zip(nSets, filenamesB))
-
-    for i in range(len(nSets)):
-        first_interpolation(dataA[i][0], dataA[i][1])
-        second_interpolation(dataB[i][0], dataB[i][1])
+    try:
+        numberF = int(input("Podaj numer funkcji ( 1 lub 2) : "))
+    except ValueError:
+        print("ValueError")
+        sys.exit(1)
         
-    print("Wyniki zapisano do plików!")
+
+    if numberF == 1:
+        calculate_points1()
+    elif numberF == 2:
+        calculate_points2()
+    
+
+
 if __name__ == "__main__":
     main()
